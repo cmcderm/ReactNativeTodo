8dfc7652087fc6289ea1d3449b53f268fd6bcbd2
Connor McDermott <connormcderm@protonmail.com>
2022-10-16 03:48:33 +0000

    Bottom tab navigation

--- App.tsx
+++ App.tsx
@@ -1,14 +1,20 @@
 import React from "react";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, View } from "react-native";
-import TodoList from "./components/todolist";
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
 export default function App() {
+
+  const Tab = createBottomTabNavigator();
+
   return (
-    <View style={styles.container}>
-      <TodoList />
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name={"Tasks"} component={TaskScreen}></Tab.Screen>
+        <Tab.Screen name={"Calendar"} component={CalendarScreen}></Tab.Screen>
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
 
